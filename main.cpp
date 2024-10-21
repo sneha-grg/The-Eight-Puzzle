644f23529a53e1235a1faa596a1760fca3cb244d
Sneha Gurung <snehagurung@Snehas-MacBook-Air.local>
2024-10-21 11:39:22 -0700

    Adding template files

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World";
+    cout << "Welcome to XXX (change this to your student ID) 8 puzzle solver.\n";
+    cout << "Type “1” to use a default puzzle, or “2” to enter your own puzzle.\n";
     return 0;
 }
